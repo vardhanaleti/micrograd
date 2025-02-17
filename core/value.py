979781a55c66bb6b4f0e2225445abd37b8e410a8
vardhanaleti <vardhanaleti2001@gmail.com>
2025-02-17 10:01:22 +0530

    Add more ops

--- core/value.py
+++ core/value.py
@@ -1,14 +1,30 @@
+OPS = {
+    "__add__": "+",
+    "__mul__": "*"
+}
+
 class Value:
 
-    def __init__(self, data):
+    def __init__(self, data, _children=(), _op='', label=''):
         self.data = data
+        self.grad = 0
+        self._children = set(_children)
+        self._op = _op
         
     def __repr__(self):
-        return f"Value(data={self.data})"
+        return f"Value(data={self.data}, _op={self._op}, _children={self._children})"
+    
+    def __add__(self, other):
+        return Value(self.data + other.data, (self, other), OPS.get(self.__add__.__name__))
+
+    def __mul__(self, other):
+        return Value(self.data * other.data, (self, other), OPS.get(self.__mul__.__name__))
+
     
     
 
 a = Value(1)
+b = Value(2)
 
-
-print(a)
+c = a + b
+print(c)
